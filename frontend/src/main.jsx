9d1923ca353251a583065e41e15ebbca8a31ed81
monsef1234 <monsef4566@gmail.com>
2023-05-14 05:17:03 +0100

    some changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 import { AppProvider } from "./context/context";
-axios.defaults.baseURL = import.meta.env.VITE_URL;
+axios.defaults.baseURL = "https://airbnb-9b1e.onrender.com";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
